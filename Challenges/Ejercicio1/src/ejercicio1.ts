2daf538e49756ca46f9637b07029966f996b408d
Sara Casasola <scasasolaborrego@sequel.com>
2020-06-07 18:43:52 +0100

    Challenges: Ejercicio 1

---
+++ Challenges/Ejercicio1/src/ejercicio1.ts
@@ -0,0 +1,27 @@
+/*************************
+ * 
+ * APARTADO A 
+ * 
+ *************************/
+
+const flatArray = arrayToFlat => arrayToFlat.flat(Infinity);
+
+/************************/
+
+/*************************
+ * 
+ * APARTADO A 
+ * 
+ *************************/
+
+interface NestedArray<T> extends Array<NestedArray<T> | T> {}
+
+function flatTypedArray<T>(arrayToFlat: Array<NestedArray<T> | T>): Array<T> {
+  return arrayToFlat.flat(Infinity);
+}
+
+/************************/
+
+const sample = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+console.log("Apartado A: ", flatArray(sample).toString());
+console.log("Apartado B: ", flatTypedArray<number>(sample).toString());
